3a391f9830994b5cf71820c05a760deb4f720284
jalilidriss111-hub <jalilidriss111@gmail.com>
2026-09-10 13:57:42 +0100

    Update route.ts

--- piecesdz-app-production/app/auth/callback/route.ts
+++ piecesdz-app-production/app/auth/callback/route.ts
@@ -23,9 +23,9 @@ export async function GET(request: Request) {
     );
   }
 
-  const response = NextResponse.redirect(
-    new URL("/dashboard", request.url)
-  );
+ const response = NextResponse.redirect(
+  "https://pieces-dz.onrender.com/dashboard"
+);
 
   response.headers.set("Cache-Control", "no-store");
 
