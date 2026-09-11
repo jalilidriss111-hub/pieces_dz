e588b62751b60f34613e33230e36978d82c79410
jalilidriss111-hub <jalilidriss111@gmail.com>
2026-09-11 11:06:24 +0100

    Update route.ts

--- piecesdz-app-production/app/auth/callback/route.ts
+++ piecesdz-app-production/app/auth/callback/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
   const SITE_URL = "https://pieces-dz.onrender.com";
 
   if (code) {
-    const supabase = createClient();
+    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
